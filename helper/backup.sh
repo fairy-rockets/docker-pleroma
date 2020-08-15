b1c97faacc36606080685dbe2e0d36aec5d8e36f
Kaede Fujisaki <kaede@hexe.net>
2020-08-16 00:09:00 +0900

    rename backup filename

--- helper/backup.sh
+++ helper/backup.sh
@@ -4,8 +4,9 @@ PROJ_PATH=$(readlink -f $(cd $(dirname $(readlink -f $0)) && pwd))
 cd ${PROJ_PATH}/..
 
 docker-compose down
+BACKUP_FILENAME="backup-$(date '+%Y%m%d').tar.gz"
 sudo bash -exu <<EOF
-tar -czvf "data-$(date '+%Y%m%d').tar.gz" ./data
-chown $(id -g):$(id -u) "data-$(date '+%Y%m%d').tar.gz"
+tar -czvf "${BACKUP_FILENAME}" ./data
+chown $(id -g):$(id -u) "${BACKUP_FILENAME}"
 EOF
 docker-compose up -d
