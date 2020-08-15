f47e9decea14a47d299ba2d5a75c6ab901ab7f29
Kaede Fujisaki <kaede@hexe.net>
2020-08-16 00:13:18 +0900

    exec sudo asap

--- helper/backup.sh
+++ helper/backup.sh
@@ -1,12 +1,13 @@
 #! /bin/bash -eu
 
+BACKUP_FILENAME="backup-$(date '+%Y%m%d').tar.gz"
+
 PROJ_PATH=$(readlink -f $(cd $(dirname $(readlink -f $0)) && pwd))
 cd ${PROJ_PATH}/..
 
-docker-compose down
-BACKUP_FILENAME="backup-$(date '+%Y%m%d').tar.gz"
 sudo bash -exu <<EOF
+docker-compose down
 tar -czvf "${BACKUP_FILENAME}" ./data
 chown $(id -g):$(id -u) "${BACKUP_FILENAME}"
-EOF
 docker-compose up -d
+EOF
